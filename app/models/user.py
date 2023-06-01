483d220f870860bed01c77fcadcf4d9b9431a487
Taylor <koo2true@gmail.com>
2023-06-01 10:35:21 -0700

    need to add relations to user

--- app/models/user.py
+++ app/models/user.py
@@ -7,3 +7,5 @@ class User(db.Model,UserMixin):
 
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(35), nullable= False, unique= True)
+    email = db.Column(db.String(150), nullable = False, unique = True)
+    password = db.Column (db.String(150), nullable = False)
