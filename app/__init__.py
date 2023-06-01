a7cd9121230d8d4aa006676471a071e3b0fd2a02
Taylor <koo2true@gmail.com>
2023-06-01 10:23:56 -0700

    added bp for anime

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from .models import db
 from .config import Config
-
+from .routes.user import user
+from .routes.anime import anime
 
 app = Flask (__name__)
 
 app.config.from_object(config)
+
+app.register_blueprint(user,url_prefix='/api/user')
+app.register_blueprint(anime,url_prefix='/api/anime')
--- app/routes/anime.py
+++ app/routes/anime.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify, redirect, request
 from flask_login import login_required
 from app.models import db
 
-user = Blueprint('user', __name__)
+anime = Blueprint('anime',__name__)
 
-@user.route('/signin')
+@anime.route('/')
+
