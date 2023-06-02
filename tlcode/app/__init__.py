c1de01fae871ec7dc42a08bc3714edc012a8f297
danishprasla <danishprasla@hotmail.com>
2023-06-02 14:50:07 -0500

    testing starter

---
+++ tlcode/app/__init__.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from .models.models import db
+from .config import Config
+from .routes.user import user
+from .routes.anime import anime
+
+app = Flask (__name__)
+
+app.config.from_object(Config)
+
+app.register_blueprint(user,url_prefix='/api/user')
+app.register_blueprint(anime,url_prefix='/api/anime')
