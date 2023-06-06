b48dd8030e84ab2a4bcf00ad781d3aa6164376dd
danishprasla <danishprasla@hotmail.com>
2023-06-06 11:46:43 -0500

    user reducer set up along with route for review post

--- react-app/src/components/CreateReview/index.js
+++ react-app/src/components/CreateReview/index.js
@@ -1,14 +1,13 @@
 import { useState,useEffect } from "react"
 import { useModal } from "../../context/Modal"
 import { useDispatch} from "react-redux"
-import {useParams} from 
+import {useParams} from 'react-router-dom'
 
 const CreateReview = ({user}) =>{
-
+    const {animeId} = useParams()
     return(
-
         <div>
-            
+
         </div>
     )
 
