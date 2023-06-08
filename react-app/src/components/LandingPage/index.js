7870fd467196242fc9667ee97334cb4b71ede99a
Kevin Huang <kevinhuang1208@gmail.com>
2023-06-08 12:53:14 -0400

    css to landing page

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -5,6 +5,7 @@ import SignupFormPage from "../SignupFormPage"
 import OpenModalButton from "../OpenModalButton";
 import LoginFormModal from '../LoginFormModal';
 import './Landingpage.css';
+import { useHistory } from 'react-router-dom';
 
 
 function LandingPage() {
@@ -13,19 +14,37 @@ function LandingPage() {
 //   openLoginModal = () =>{
 
 //   }
+  const history = useHistory()
+
+  const handleClick = (e) => {
+    e.preventDefault()
+    history.push('/anime')
+
+  }
 
   return (
     <div className='landingPageContainer'>
       <img className="landingPageLogo" src="https://cdn.discordapp.com/attachments/1113213089702228038/1115025363232378891/phillyroll-low-resolution-logo-color-on-transparent-background.png"/>
+      <div className='wholeFormContainer'>
       <div className='landingPageSignUpContainer'>
         <SignupFormPage />
       </div>
 	  <div className='landingPageLoginContainer'>
-		<OpenModalButton 
+		<OpenModalButton
+      className='button'
 			buttonText="Already have an account? Log In."
 			modalComponent={<LoginFormModal/>}
-		/>	
+		/>
 	  </div>
+    <div>
+        <p>
+          Continue without logging in:
+        </p>
+        <p>
+          <button onClick={(e) => handleClick(e)}> Guest View</button>
+        </p>
+      </div>
+    </div>
     </div>
 
 
